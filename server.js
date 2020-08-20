a8919a7718fef0bcad77197f470b73605e173c8d
Tejas Ghanwat <tejas.aghanwat@gmail.com>
2020-08-19 20:11:28 -0700

    add routes

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-//Connect Database
+//Connect database
+
 connectDB();
 
-app.get("/", (req, res) => res.send("API Running"));
+// Init Middleware
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API running"));
 
 //Define Routes
 
-// app.use("/api/users", require("./routes/api/users"));
-// app.use("/api/auth", require("./routes/api/auth"));
-// app.use("/api/profile", require("./routes/api/profile"));
-// app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profiles", require("./routes/api/profiles"));
+app.use("/api/posts", require("./routes/api/posts"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.port || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
